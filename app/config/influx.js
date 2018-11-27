ed9aa25cc9768d84445dfd0f36ce9303074d7a43
Macelo Costa <macelocosta@hotmail.com>
2018-11-27 18:40:56 -0300

    bcrypt ok

--- app/config/influx.js
+++ app/config/influx.js
@@ -26,15 +26,12 @@ module.exports = function() {
 
   influx.getDatabaseNames()
   .then(names => {
+    console.log("InfluxDB connection estabilished");
     if (!names.includes(database_name)) {
       let db = influx.createDatabase(database_name);
       influx.createRetentionPolicy('solidbits', {duration: 'inf', replication: 1, database: database_name, isDefault: 1});
       return db;
     }
-  });
-
-
-  console.log("InfluxDB connection estabilished");
-  
+  });  
   return influx;
 }
