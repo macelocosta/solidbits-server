7ba03e1461e778fc93d92b831c0cd83343c0ed13
Macelo Costa <macelocosta@hotmail.com>
2018-11-27 18:13:36 -0300

    'bcrypt'

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt-nodejs');
 
 var UserSchema = new mongoose.Schema({
   name: {
@@ -34,7 +34,6 @@ var UserSchema = new mongoose.Schema({
   timestamps: true
 });
 
-// hasheia a senha antes de salvar no bd
 UserSchema.pre('save', function(next){
   var user = this;
   var salt_factor = 10;
