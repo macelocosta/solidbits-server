70cef66db9bb64d1229b9ac0c3c17cde7be53891
Macelo Costa <macelocosta@hotmail.com>
2018-11-30 09:22:42 -0300

    business added

--- app/routes/api.routes.js
+++ app/routes/api.routes.js
@@ -3,6 +3,7 @@ const passport = require('passport');
 const passportSvc = require('./../config/passport');
 const authController = require ('./../controllers/auth.controller');
 const dataController = require('./../controllers/data.controller');
+const businessController = require('./../controllers/business.controller');
 
 const router = express.Router();
 
@@ -27,7 +28,15 @@ router.get('/data/overview/monitoring', dataController.getOverviewMonitoring);
 router.get('/data/overview/current-weight', dataController.getCurrentWeight);
 router.get('/data/overview/current-volume', dataController.getCurrentVolume);
 router.get('/data/overview/bins-by-fill-level', dataController.getBinsByFillLevel);
-router.get('/biggest-waste-producers', dataController.getBiggestWasteProducers);
+router.get('/data/biggest-waste-producers', dataController.getBiggestWasteProducers);
+
+//business route
+router.get('/business', businessController.getBusiness);
+router.post('/business', businessController.updateBusiness);
+
+// TODO: colocar os requireAuth!!!
+// TODO: colocar os requireAuth!!!
+// TODO: colocar os requireAuth!!!
 
 // todoRoutes.get('/', requireAuth, AuthenticationController.roleAuthorization(['reader','creator','editor']), TodoController.getTodos);
 // todoRoutes.post('/', requireAuth, AuthenticationController.roleAuthorization(['creator','editor']), TodoController.createTodo);
